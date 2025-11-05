8f702d31d419c83ccb9f7b6126e9e524dc754eee
Ej <ernest@oddpluseven.com>
2025-11-05 12:02:44 -0400

    1203

---
+++ framer_code_component.tsx
@@ -0,0 +1,18 @@
+// Framer Code Component - Adventure Directory
+// Paste this into a Code Component in Framer
+
+export default function AdventureDirectory() {
+  return (
+    <iframe
+      src="https://ernestdarrow.github.io/nelsoncounty/frontpage_framer.html"
+      style={{
+        width: '100%',
+        height: '100%',
+        border: 'none',
+      }}
+      loading="lazy"
+      title="Adventure Directory"
+    />
+  );
+}
+
